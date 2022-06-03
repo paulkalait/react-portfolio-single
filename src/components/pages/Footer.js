dbd5c916753ffb43f72a27c21f73aad7e65b6527
Your Name <kalaitzidispaul@gmail.com>
2022-06-03 17:14:59 -0400

    styled icons

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -1,31 +1,34 @@
 import React from 'react'
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { FaGithub, FaLinkedin, FaGoogle } from "react-icons/fa";
+import CV from "../../assets/Paul's CV 2022.pdf"
+
+
 
 const Footer = () => {
   return (
-    <div className='sm: shadow-lg w-full h-16 justify-center bg-slate-300 fixed bottom-0 '>
-    <div className='flex justify-center flex-row items-center'>
-    <div className="my-3 mx-4  mt-20 ">
+    <div className='sm: shadow-lg w-full  justify-center bg-slate-300 '>
+    <div className='flex justify-center flex-row items-center icons-footer'>
+    <div className="my-3 mx-4  mt-5 ">
     <a
       href="https://www.linkedin.com/in/paul-kalaitzidis-393555196"
       className="linkedin"
     >
-      <FaLinkedin />
+    <FaLinkedin />
     </a>
   </div>
-  <div className="my-3 mx-4 mt-20">
+  <div className="my-3 mx-4 mt-5">
     <a href="https://github.com/paulkalait" className="github">
       <FaGithub />
     </a>
   </div>
-  <div className="my-3 mx-4  mt-20">
+  <div className="my-3 mx-4  mt-5">
     <a href="mailto:kalaitzidispaul@gmail.com" className="google">
       <FaGoogle />
     </a>
   </div>
-  <div className="my-3 mx-4 mt-20">
-    <a className="resume">
+  <div className="my-3 mx-4 mt-5">
+    <a href={CV} className="resume">
       <BsFillPersonLinesFill />
     </a>
   </div>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
     >
       <div className="max-w-[1000px] mx-auto flex flex-row justify-center h-full">
         <div className="mt-52">
-          <img src={PHOTO} alt="profile-photo" className=" ml-3 sm: w-56 w-56 md:w-48 h-48 profile" />
+          <img src={PHOTO} alt="profile-photo" className=" ml-3 sm: w-56 w-56 md:w-48 h-48 profile drop-shadow" />
           <div className="mt-5">
             <h1 className="font-extrabold text-3xl">Hi Im Paul</h1>
             <div className="font-bold text-xl">
